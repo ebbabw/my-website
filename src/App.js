7a3a28e8c331c21719ca48a965ee9afba4037a13
ebbabw <ebbabergholtz@gmail.com>
2020-09-15 16:26:04 +0200

    first changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { quiz } from 'reducers/quiz'
 
-import { CurrentQuestion } from 'components/CurrentQuestion'
 
-const reducer = combineReducers({
-  quiz: quiz.reducer
-})
+import { Homepage } from 'pages/Homepage'
 
-const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <CurrentQuestion />
-    </Provider>
+
+      <Homepage />
+
   )
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Menu } from 'components/Menu'
+
+
+export const Homepage = () => {
+
+
+  return (
+    <div>
+      <h1>TJOOO TJOO</h1>
+      <Menu />
+    </div>
+  )
+}
